40ec26f79235887b21ccabfbd8eda86a75940910
vvvvvx <viaco.xu@gmail.com>
2025-02-13 13:46:32 +0800

    init

---
+++ init.lua
@@ -0,0 +1,9 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+
+if vim.g.neovide then
+  vim.o.guifont = "Source Code Pro:h12"
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_scale_factor = 1.0
+  --	vim.g.neovide_transparency = 0.9
+end
+require("config.lazy")
